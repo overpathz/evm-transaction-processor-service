8cf4877f814fc082eea92309259a72cabb7f7b67
overpathz <overpathz@gmail.com>
2024-10-30 11:50:34 +0200

    Project draft #1

---
+++ src/main/java/com/overpathz/evmtransactionprocessorservice/EvmTransactionProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.overpathz.evmtransactionprocessorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvmTransactionProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvmTransactionProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/overpathz/evmtransactionprocessorservice/EvmTransactionProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overpathz.evmtransactionprocessorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvmTransactionProcessorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
